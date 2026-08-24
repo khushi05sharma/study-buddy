7c252ea34b6c5a89f8a5470fc139136f26b4ab57
Khushi Sharma <khushisharma.50031@gmail.com>
2026-08-24 23:42:16 +0530

    added schema structure

--- backend/src/models/Document.ts
+++ backend/src/models/Document.ts
@@ -1,7 +1,29 @@
-import mongoose, { Schema, Document as MongooseDocument } from "mongoose";
+import mongoose, {
+  Schema,
+  Document as MongooseDocument,
+  model,
+} from "mongoose";
 
 export interface IDocument extends MongooseDocument {
   title: string;
   content: string;
   createdAt: Date;
 }
+
+const DocumentSchema = new Schema<IDocument>({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IDocument>("Document", DocumentSchema);
