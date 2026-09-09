773be4c3381c14c9a35dff5647e496dbf4981dfc
Khushi Sharma <khushisharma.50031@gmail.com>
2026-09-10 01:07:28 +0530

    added comments

--- backend/src/controllers/chatController.ts
+++ backend/src/controllers/chatController.ts
@@ -5,6 +5,7 @@ import ChatModel from "../models/Chat";
 
 export const getChatHistory = async (req: Request, res: Response) => {
   try {
+    // Find chat records in MongoDB.Sort by newest first.Don't return more than 50 chat records.
     const chats = await ChatModel.find().sort({ createdAt: -1 }).limit(50);
 
     return res.status(200).json(chats);
